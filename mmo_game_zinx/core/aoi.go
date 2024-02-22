010d71866eeeb90aa8332b238cbb831ef9943823
advancedt <1875081903@qq.com>
2024-02-22 18:20:52 -0500

    ZINX_Finished

--- mmo_game_zinx/core/aoi.go
+++ mmo_game_zinx/core/aoi.go
@@ -84,12 +84,83 @@ func (m *AOIManager) GetSurroundGridsByGid(gID int) (grids []*Grid) {
 		return
 	}
 	// 初始化grids返回值切片，将当前gid本身加入到九宫格中
-	grids = append()
+	grids = append(grids, m.grids[gID])
 	// 需要通过gID得到当前格子x轴的编号 nx = id % nx
-
+	idx := gID % m.CntsX
 	// gID的左边是否有格子 || 右边是否有格子 --放到gidsX中
-
+	if idx > 0 {
+		grids = append(grids, m.grids[gID-1])
+	}
+	if idx < m.CntsX-1 {
+		grids = append(grids, m.grids[gID+1])
+	}
 	// 遍历giddX集合中每个格子的gid
-	// 上面是否有格子
-	// 下面是否有格子
+	gidsX := make([]int, 0, len(grids))
+	for _, v := range grids {
+		gidsX = append(gidsX, v.GID)
+	}
+	for _, v := range gidsX {
+		// 得到当前格子id的Y轴编号
+		idy := v / m.CntsY
+		// 上面是否有格子
+		if idy > 0 {
+			grids = append(grids, m.grids[v-m.CntsX])
+		}
+		// 下面是否有格子
+		if idy < m.CntsY-1 {
+			grids = append(grids, m.grids[v+m.CntsX])
+		}
+	}
+	return
+}
+
+// 通过横纵轴坐标得到当前格子编号
+func (m *AOIManager) GetGidByPos(x, y float32) int {
+	idx := (int(x) - m.MinX) / m.gridWidth()
+	idy := (int(y) / m.MinY) / m.gridLength()
+	return idy*m.CntsX + idx
+}
+
+// 通过横纵坐标，得到周边九宫格内全部的playerIDs
+func (m *AOIManager) GetPidsByPos(x, y float32) (playerIDs []int) {
+	// 得到当前玩家的GID格子
+	gId := m.GetGidByPos(x, y)
+	// 通过GID的得到周边九宫格信息
+	grids := m.GetSurroundGridsByGid(gId)
+	// 将九宫格的信息里的全部player累加到playerIDs
+	for _, grid := range grids {
+		playerIDs = append(playerIDs, grid.GetPlayerIDs()...)
+		fmt.Sprintf("====> grid ID: %d, pids: %v ====", grid.GID, grid.GetPlayerIDs())
+	}
+	return
+}
+
+// 添加一个playerID到一个格子中
+func (m *AOIManager) AddPidToGrid(pID, gID int) {
+	m.grids[gID].Add(pID)
+}
+
+// 移除一个格子中的playerID
+func (m *AOIManager) RemovePidFromGid(pID, gID int) {
+	m.grids[gID].Remove(pID)
+}
+
+// 通过GID获得全部的PlayerID
+func (m *AOIManager) GetPidsByGid(gID int) (playerIDs []int) {
+	playerIDs = m.grids[gID].GetPlayerIDs()
+	return
+}
+
+// 通过坐标将Player添加到一个格子中
+func (m *AOIManager) AddToGridByPos(pID int, x, y float32) {
+	gID := m.GetGidByPos(x, y)
+	grid := m.grids[gID]
+	grid.Add(pID)
+}
+
+// 通过坐标将Player从一个格子中删除
+func (m *AOIManager) RemoveFromGridByPos(pID int, x, y float32) {
+	gID := m.GetGidByPos(x, y)
+	grid := m.grids[gID]
+	grid.Remove(pID)
 }
--- mmo_game_zinx/core/aoi_test.go
+++ mmo_game_zinx/core/aoi_test.go
@@ -11,3 +11,18 @@ func TestNewAOIManager(t *testing.T) {
 	// 打印AOIManager
 	fmt.Println(aoiMgr)
 }
+
+func TestAOIManagerSurroundGridsByGid(t *testing.T) {
+	aoiMgr := NewAOIManager(0, 250, 5, 0, 250, 5)
+
+	for gid, _ := range aoiMgr.grids {
+		// 得到当前gid的周边九宫格的信息
+		grids := aoiMgr.GetSurroundGridsByGid(gid)
+		fmt.Println("Current Gid:", gid, "gids len =", len(grids))
+		gIDs := make([]int, 0, len(grids))
+		for _, grid := range grids {
+			gIDs = append(gIDs, grid.GID)
+		}
+		fmt.Println("surrounding grid IDs are:", gIDs)
+	}
+}
